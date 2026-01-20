00ee4897474ea2a5e072154fb9f9e3ccf762d803
willo54321 <williamj.neale@outlook.com>
2026-01-20 14:56:04 +0000

    Updates

--- _next/static/xW9hQMt4IAU0owThOLgng/_buildManifest.js
+++ _next/static/xW9hQMt4IAU0owThOLgng/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/consultation-suite//_next/:path+",
+        "destination": "/consultation-suite/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
